c61f23191116e407c5a9cf071513f8a32ec541c5
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2025-09-30 19:59:38 +0530

    database name fixed

--- backend/models/studentProfile.js
+++ backend/models/studentProfile.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "StudentProfile",
-      tableName: "studentprofiles",
+      tableName: "student_profiles",
       timestamps: true,
       underscored: true,
     }
